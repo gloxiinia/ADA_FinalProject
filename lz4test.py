613838c16e8723f7c70526e1be8bd46cb0079b75
gloxiinia <rachelanastasia.w@gmail.com>
2022-12-07 00:05:56 +0700

    polished the printed output

--- lz4test.py
+++ lz4test.py
@@ -9,54 +9,108 @@ import os
 
 img = image.imread("testFiles/kirby.jpg")
 
+#--------------------------------------------------------------------------------------------------#
+
 #TEXT FILE
 
-f=open("testFiles/theraven.txt", "rb")
+print("-------------------TXT FILE----------------------")
+with open('testFiles/theraven.txt', 'rb') as infile:
+    with open('testFiles/theraven.lz4', 'wb') as outfile:
+        outfile.write(lz4.frame.compress(infile.read()))
 
-if f.mode == 'rb':
-    book = f.read()
-else:
-    print("File cannot be read, sorry!")
+with open('testFiles/theraven.lz4', 'rb') as infile:
+    with open('testFiles/theravenAfterlz4.txt', 'wb') as outfile:
+        outfile.write(lz4.frame.decompress(infile.read()))
 
+#file size comparison
 
-#TEST TIME FOR TEXT FILE
-st = time.time()
+#original
+file_stats = os.stat("testFiles/theraven.txt")
+print("Original file:")
+print(f'File Size in Bytes is {file_stats.st_size}')
+print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
 
-cdata = lz4.frame.compress(book)
+#compressed
+file_stats = os.stat("testFiles/theraven.lz4")
+print("Compressed file:")
+print(f'file size in Bytes is {file_stats.st_size}')
+print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
 
-et = time.time()
-elapsed_time = et - st
+#decompressed
+file_stats = os.stat("testFiles/theravenAfterlz4.txt")
+print("Decompressed file:")
+print(f'file size in Bytes is {file_stats.st_size}')
+print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
 
-print('Execution time:', elapsed_time, 'seconds')
-
-
-print("The size of the compressed file is:", sys.getsizeof(cdata))
-
-decomdata = lz4.frame.decompress(cdata)
-
-print("The size of the decompressed file is:", sys.getsizeof(decomdata))
+#--------------------------------------------------------------------------------------------------#
 
 #CSV FILE
 
 print("-------------------CSV FILE----------------------")
-with open('testFiles/movies_metadata.csv', 'rb') as infile:
-    with open('testFiles/movies_metadata.lz4', 'wb') as outfile:
+with open('testFiles/movies.csv', 'rb') as infile:
+    with open('testFiles/movies.lz4', 'wb') as outfile:
         outfile.write(lz4.frame.compress(infile.read()))
 
-with open('testFiles/movies_metadata.lz4', 'rb') as infile:
-    with open('testFiles/movies_metadataAfterlz4.csv', 'wb') as outfile:
+with open('testFiles/movies.lz4', 'rb') as infile:
+    with open('testFiles/moviesAfterlz4.csv', 'wb') as outfile:
         outfile.write(lz4.frame.decompress(infile.read()))
+
+#file size comparison
+
+#original
+file_stats = os.stat("testFiles/movies.csv")
+print("Original file:")
+print(f'File Size in Bytes is {file_stats.st_size}')
+print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
+
+#compressed
+file_stats = os.stat("testFiles/movies.lz4")
+print("Compressed file:")
+print(f'file size in Bytes is {file_stats.st_size}')
+print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
+
+#decompressed
+file_stats = os.stat("testFiles/moviesAfterlz4.csv")
+print("Decompressed file:")
+print(f'file size in Bytes is {file_stats.st_size}')
+print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
+
+#--------------------------------------------------------------------------------------------------#
 
 #PDF FILE
 
+#compression
 print("-------------------PDF FILE----------------------")
 with open('testFiles/covidindo.pdf', 'rb') as infile:
     with open('testFiles/covidindo.lz4', 'wb') as outfile:
         outfile.write(lz4.frame.compress(infile.read()))
 
+#decompression
 with open('testFiles/covidindo.lz4', 'rb') as infile:
     with open('testFiles/covidindoAfterlz4.pdf', "wb") as outfile:
         outfile.write(lz4.frame.decompress(infile.read()))
+
+#file size comparison
+
+#original
+file_stats = os.stat("testFiles/covidindo.pdf")
+print("Original file:")
+print(f'File Size in Bytes is {file_stats.st_size}')
+print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
+
+#compressed
+file_stats = os.stat("testFiles/covidindo.lz4")
+print("Compressed file:")
+print(f'file size in Bytes is {file_stats.st_size}')
+print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
+
+#decompressed
+file_stats = os.stat("testFiles/covidindoAfterlz4.pdf")
+print("Decompressed file:")
+print(f'file size in Bytes is {file_stats.st_size}')
+print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
+
+#--------------------------------------------------------------------------------------------------#
 
 #JSON FILE
 
@@ -77,19 +131,35 @@ with open('testFiles/example_2.lz4', 'rb') as infile:
 file_stats = os.stat("testFiles/example_2.json")
 print("Original file:")
 print(f'File Size in Bytes is {file_stats.st_size}')
-print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
+print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
 
 #compressed
 file_stats = os.stat("testFiles/example_2.lz4")
 print("Compressed file:")
 print(f'file size in Bytes is {file_stats.st_size}')
-print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
+print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
 
 #decompressed
 file_stats = os.stat("testFiles/example_2Afterlz4.json")
 print("Decompressed file:")
 print(f'file size in Bytes is {file_stats.st_size}')
-print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
+print(f'file size in MegaBytes is {file_stats.st_size / (1024 * 1024)}\n')
 
 
 
+# #TEST TIME FOR TEXT FILE
+# st = time.time()
+
+# cdata = lz4.frame.compress(book)
+
+# et = time.time()
+# elapsed_time = et - st
+
+# print('Execution time:', elapsed_time, 'seconds')
+
+
+# print("The size of the compressed file is:", sys.getsizeof(cdata))
+
+# decomdata = lz4.frame.decompress(cdata)
+
+# print("The size of the decompressed file is:", sys.getsizeof(decomdata))
